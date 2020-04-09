727c97a563ef2a3bf412e74817d566fab7190b3a
Don Namgyal <dhondup.namgyal@gmail.com>
2020-04-09 16:36:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,24 +4,15 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   updated_data = {}
-    data.each do |color_gender_location, value_hash|
-      value_hash.each do |attributes, value_array|
-        value_array.each do |name|
-          updated_data[name] ||= {}
-          updated_data[name][color_gender_location] ||= []
-          updated_data[name][color_gender_location].push(attributes.to_s)
-          # if !updated_data[name]
-          #   updated_data[name] = {}
-          # end
-          # if !updated_data[name][color_gender_location]
-          #   updated_data[name][color_gender_location] = []
-          # end
-          # if !updated_data[name][color_gender_location].include?(attributes)
-          #   updated_data[name][color_gender_location].push(attributes.to_s)
-          # end
-        end
+  data.each do |color_gender_location, value_hash|
+    value_hash.each do |attributes, value_array|
+      value_array.each do |name|
+        updated_data[name] ||= {}
+        updated_data[name][color_gender_location] ||= []
+        updated_data[name][color_gender_location].push(attributes.to_s)
       end
     end
-    updated_data
+  end
+  updated_data
 end
 

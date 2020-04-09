0cfac40d9513f59236115987ecaefedf51bfc007
Don Namgyal <dhondup.namgyal@gmail.com>
2020-04-09 16:36:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,20 +3,6 @@ require 'pry'
 
 
 def nyc_pigeon_organizer(data)
-  # write your code here!
 
-  new_data = {}
-  data.each do |attributes, attr_hash|
-    attr_hash.each do |subcat, names|
-      names.each do |name|
-        if !new_data.key?(name)
-          new_data[name] = {color: {}, gender: {}, lives: {}}
-        end
-      end
-    end
-    binding.pry
-
-  end
-  new_data
 end
 
